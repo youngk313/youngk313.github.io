15f3ae8a6ffabc15b9665b64d98def2ea5e040b6
Young <youngkwon313@gmail.com>
2021-02-03 11:25:00 -0800

    added port

--- COMP4537/labs/4/server.js
+++ COMP4537/labs/4/server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const url = require('url');
+const port = process.env.PORT || 8080
+
 http.createServer(function (req, res) {
 	const query = url.parse(req.url, true);
 	res.writeHead(200, {"Content-Type": "text/html"});
 	res.end('Hello ' + query.query["text"]);
-}).listen(8080);
+}).listen(port);
