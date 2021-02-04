98ac0c793f8a918af450fed1253a80e9bebac394
Young <youngkwon313@gmail.com>
2021-02-03 19:01:14 -0800

    get full URL

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ http.createServer(function (req, res) {
         filePath = './index.html';
     } else if (filePath.includes('./COMP4537/labs/4/writeFile')) {
         const query = url.parse(req.url, true);
-        const newText = __dirname + "\nURL: " + req.url + "\ntext: " + query.query["text"] + "\n"
+        const newText = "\nURL: " + req.protocol + '://' + req.headers.host + req.url + "\ntext: " + query.query["text"] + "\n"
+        console.log(newText)
         fs.appendFile(readFileURL + "/file.txt", newText, function(err, data) {
             if (err) {
                 res.writeHead(404, {
