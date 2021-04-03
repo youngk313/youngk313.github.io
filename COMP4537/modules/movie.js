d22cd852f1e53237b5ee767e7947de2373097528
Young <youngkwon313@gmail.com>
2021-04-03 16:54:13 -0700

    added CORS

--- COMP4537/modules/movie.js
+++ COMP4537/modules/movie.js
@@ -129,6 +129,13 @@ function deleteMovieById(connection, response, id) {
     console.log("Deletion completed!");
 }
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
+
 app.get(endPoint + "movie",  function(req, res) {
     console.log('Getting movies');
     getMovies(connection, res);
